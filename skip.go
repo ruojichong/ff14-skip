d3cd510714688131fd98ab7b418ddd20ff734d1a
ruojichong <d00000.0@qq.com>
2022-03-23 00:48:51 +0800

    5

--- skip.go
+++ skip.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	time1 := time.Now().Unix()
-	cmd := exec.Command("runas", "/trustlevel:0x40000", "cmd", "/C", "date 2099/09/22")
+	cmd := exec.Command("cmd", "/C", "date 2099/09/22")
 	cmd.Run()
 	time.Sleep(1 * time.Second)
 	cmd2 := exec.Command("cmd", "/C", "date "+time.Unix(time1, 0).Format("2006/01/02"))
