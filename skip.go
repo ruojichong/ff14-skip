a4de9101559780f57455e50b35f9b2af355bda0f
ruojichong <d00000.0@qq.com>
2022-03-23 10:58:00 +0800

    修复了bug

--- skip.go
+++ skip.go
@@ -11,8 +11,10 @@ func main() {
 
 	fmt.Println("请以管理员权限运行")
 	fmt.Println("有动画时按任意键并回车,即可通过掉线跳过当次动画")
-	for {
-		_, _ = fmt.Scanln()
+	var x string
+	for i := 1; ; i++ {
+		fmt.Printf("请输入并回车,这是第%d次跳过\n", i)
+		_, _ = fmt.Scan(&x)
 		time1 := time.Now().Unix()
 		cmd := exec.Command("cmd", "/C", "date 2099/09/24")
 		err1 := cmd.Run()
@@ -24,6 +26,7 @@ func main() {
 		time.Sleep(1 * time.Second)
 		cmd2 := exec.Command("cmd", "/C", "date "+time.Unix(time1, 0).Format("2006/01/02"))
 		_ = cmd2.Run()
+		fmt.Println("已经完成一次跳过")
 	}
 
 }
