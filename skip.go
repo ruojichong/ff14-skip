0ba6ceeb060f98639c075c460383fcfef02fa411
ruojichong <d00000.0@qq.com>
2022-03-23 10:47:09 +0800

    提示人性化

--- skip.go
+++ skip.go
@@ -2,27 +2,28 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"os/exec"
 	"time"
 )
 
 func main() {
 
-	var x string
 	fmt.Println("请以管理员权限运行")
 	fmt.Println("有动画时按任意键并回车,即可通过掉线跳过当次动画")
 	for {
-		fmt.Scanln(&x)
+		_, _ = fmt.Scanln()
 		time1 := time.Now().Unix()
 		cmd := exec.Command("cmd", "/C", "date 2099/09/24")
 		err1 := cmd.Run()
 		if err1 != nil {
-			fmt.Println("使用管理员权限运行,按任意键退出")
-			time.Sleep(10 * time.Second)
+			fmt.Println("未管理员权限运行,程序将在3秒后退出")
+			time.Sleep(3 * time.Second)
+			os.Exit(1)
 		}
 		time.Sleep(1 * time.Second)
 		cmd2 := exec.Command("cmd", "/C", "date "+time.Unix(time1, 0).Format("2006/01/02"))
-		cmd2.Run()
+		_ = cmd2.Run()
 	}
 
 }
