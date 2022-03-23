19132c0d028f67fb7085e42f2dd7c3e836ac434e
ruojichong <d00000.0@qq.com>
2022-03-23 10:16:39 +0800

    9

--- skip.go
+++ skip.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"os/exec"
 	"time"
 )
@@ -8,7 +9,11 @@ import (
 func main() {
 	time1 := time.Now().Unix()
 	cmd := exec.Command("cmd", "/C", "date 2099/09/24")
-	cmd.Run()
+	err1 := cmd.Run()
+	if err1 != nil {
+		fmt.Println("使用管理员权限运行,按任意键退出")
+		time.Sleep(10 * time.Second)
+	}
 	time.Sleep(1 * time.Second)
 	cmd2 := exec.Command("cmd", "/C", "date "+time.Unix(time1, 0).Format("2006/01/02"))
 	cmd2.Run()
